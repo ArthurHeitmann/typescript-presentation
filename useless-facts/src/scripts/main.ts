0582054d7ef11e7d8ee629737d9ee9cd2db41875
ArthurHeitmann <art.tec.15@gmail.com>
2021-05-26 22:29:01 +0200

    updated files

--- useless-facts/src/scripts/main.ts
+++ useless-facts/src/scripts/main.ts
@@ -1,1 +1,10 @@
-console.log("hello world");
+console.log("Hello World!");
+
+const randomButton = document.getElementById("randomFactBtn");
+const dailyButton = document.getElementById("dailyFactBtn");
+const enButton = document.getElementById("enFactBtn");
+const deButton = document.getElementById("deFactBtn");
+const reloadButton = document.getElementById("reloadBtn");
+
+const factSpan = document.getElementById("fact");
+const factLink = document.getElementById("factSrc");
